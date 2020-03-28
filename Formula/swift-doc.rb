ef04855b00a4ded9ef5b89829fc15e616adac225
Mattt <mattt@me.com>
2020-03-28 05:51:16 -0700

    Bump swift-doc to 0.1.0

--- Formula/swift-doc.rb
+++ Formula/swift-doc.rb
@@ -1,10 +1,10 @@
 class SwiftDoc < Formula
   desc "Swift documentation generator"
   homepage "https://github.com/SwiftDocOrg/swift-doc"
-  url "https://github.com/SwiftDocOrg/swift-doc.git", :tag => "0.0.4", :revision => "f852a149365e2ece4417b4e1ffc4d1094b644cb7"
+  url "https://github.com/SwiftDocOrg/swift-doc.git", :tag => "0.1.0", :revision => "3ff6fa68a704b715a9a2409f32fa9d87a9935cae"
   head "https://github.com/SwiftDocOrg/swift-doc.git", :shallow => false
 
-  depends_on :xcode => ["11.0", :build]
+  depends_on :xcode => ["11.4", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
