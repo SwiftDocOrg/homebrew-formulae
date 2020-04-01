b76a001ec76b2edcd6d084770666359b206a82bc
Mattt <mattt@me.com>
2020-04-01 09:44:17 -0700

    Update swift-doc to 1.0.0-beta.1

--- Formula/swift-doc.rb
+++ Formula/swift-doc.rb
@@ -1,7 +1,7 @@
 class SwiftDoc < Formula
   desc "Swift documentation generator"
   homepage "https://github.com/SwiftDocOrg/swift-doc"
-  url "https://github.com/SwiftDocOrg/swift-doc.git", :tag => "0.1.1", :revision => "b5274fbe4813dc6e6616007009747979315e2cfc"
+  url "https://github.com/SwiftDocOrg/swift-doc.git", :tag => "1.0.0-beta.1", :revision => "85b5872e3cdb58be273d3dd2315fa101acbfbe3c"
   head "https://github.com/SwiftDocOrg/swift-doc.git", :shallow => false
 
   depends_on :xcode => ["11.4", :build]
