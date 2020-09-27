7a2133fabd5e864fe2e5687abc35cff40104cdd6
Mattt <mattt@me.com>
2020-09-27 05:35:29 -0700

    Run `brew style --fix` on formulae

--- Formula/swift-doc.rb
+++ Formula/swift-doc.rb
@@ -1,10 +1,10 @@
 class SwiftDoc < Formula
   desc "Swift documentation generator"
   homepage "https://github.com/SwiftDocOrg/swift-doc"
-  url "https://github.com/SwiftDocOrg/swift-doc.git", :tag => "1.0.0-beta.4", :revision => "581af2fe50667ed2f49c53d26b91a8feb4531302"
-  head "https://github.com/SwiftDocOrg/swift-doc.git", :shallow => false
+  url "https://github.com/SwiftDocOrg/swift-doc.git", tag: "1.0.0-beta.4", revision: "581af2fe50667ed2f49c53d26b91a8feb4531302"
+  head "https://github.com/SwiftDocOrg/swift-doc.git", shallow: false
 
-  depends_on :xcode => ["11.4", :build]
+  depends_on xcode: ["11.4", :build]
   depends_on "graphviz" => :recommended
 
   def install
--- Formula/swift-doctest.rb
+++ Formula/swift-doctest.rb
@@ -1,10 +1,10 @@
 class SwiftDoctest < Formula
   desc "Swift documentation tester"
   homepage "https://github.com/SwiftDocOrg/DocTest"
-  url "https://github.com/SwiftDocOrg/DocTest.git", :tag => "0.1.0", :revision => "37995d5a6be780957f566535c63668bd3407c842"
-  head "https://github.com/SwiftDocOrg/swift-doc.git", :shallow => false
+  url "https://github.com/SwiftDocOrg/DocTest.git", tag: "0.1.0", revision: "37995d5a6be780957f566535c63668bd3407c842"
+  head "https://github.com/SwiftDocOrg/swift-doc.git", shallow: false
 
-  depends_on :xcode => ["11.4", :build]
+  depends_on xcode: ["11.4", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
